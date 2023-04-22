a4d03c20d212141f897d6f43c3bdd56e373f99bd
h.ohnuki <h.ohnuki.plc@gmail.com>
2023-04-23 00:10:05 +0900

    correction of expandable menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,23 @@
+var menu_close=false;
 window.onload = function() {
+	let menu_switch = document.getElementById("menu_switch");
+	let side_menu =	document.getElementById("side_menu");
 	if(window.innerWidth >= 1100){
-		document.getElementById("side_menu").setAttribute("open","open");
+		side_menu.setAttribute("open","open");
+		menu_switch.textContent = "Close menu";
+		meun_close = false;
+	}else{
+		menu_close = true;
 	}
+	menu_switch.addEventListener("click",() => {
+		if(menu_close == true){
+			menu_switch.textContent = "Close menu";
+			menu_close = false;
+		}else{
+			menu_switch.textContent = "Open menu";
+			menu_close = true;
+		}
+		console.log("menu="+menu_close);
+	});
 }
+
